259728d933a5df1c35261726d3ce108e92a8864c
Brijsunil2 <33044885+Brijsunil2@users.noreply.github.com>
2023-12-28 21:44:20 -0500

    Implement design for create forum modal

---
+++ frontend/src/components/ForumsSection/ForumCreateForm.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import * as formik from "formik";
+import * as yup from "yup";
+
+const ForumCreateForm = () => {
+  const [validate, setValidate] = useState(true);
+
+  const { Formik } = formik;
+
+  const formSchema = yup.object().shape({
+    title: yup.string().required(),
+    desc: yup.string().required(),
+  });
+
+  const submitHandler = async (values) => {
+    console.log(values);
+    setValidate(true);
+  };
+
+  return (
+    <Formik 
+      validationSchema={formSchema}
+      onSubmit={(values) => submitHandler(values)}
+      initialValues={{
+        title: "",
+        desc: "",
+      }}
+    >
+      {({ handleSubmit, handleChange, values, touched, errors }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Row className="mb-3">
+            <Form.Group as={Col}>
+              <Form.Label>Forum title</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Forum title"
+                name="title"
+                value={values.title}
+                onChange={handleChange}
+                isInvalid={errors.title && touched.title}
+              />
+              <Form.Control.Feedback type="invalid">
+                {validate ? "Please enter in a title" : ""}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col}>
+              <Form.Label>Forum description</Form.Label>
+              <Form.Control
+                type="text"
+                as="textarea" 
+                rows={3}
+                placeholder="Forum description"
+                name="desc"
+                value={values.desc}
+                onChange={handleChange}
+                isInvalid={errors.desc && touched.desc}
+              />
+              <Form.Control.Feedback type="invalid">
+                {validate ? "Please enter in a description" : ""}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Button type="submit">Create</Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default ForumCreateForm;
--- frontend/src/components/ForumsSection/ForumsSection.jsx
+++ frontend/src/components/ForumsSection/ForumsSection.jsx
@@ -1,52 +1,70 @@
 import "./ForumsSection.css";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Row, Col, Button, Modal, Form } from "react-bootstrap";
 import ForumEntry from "./ForumEntry";
 import Searchbar from "../Searchbar/Searchbar";
+import ForumCreateForm from "./ForumCreateForm";
 
 const ForumsSection = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalClose = () => setShowModal(false);
+
+  const handleModalShow = () => setShowModal(true);
+
   const searchBarSubmit = (value) => {
     console.log(value);
   };
 
   return (
-    <div className="forumssection-container">
-      <Container>
-        <h2>Forums</h2>
+    <>
+      <div className="forumssection-container">
         <Container>
-          <Row className="mt-2">
-            <Col xs={8}>
-              <Searchbar submitFunc={searchBarSubmit} />
-            </Col>
-            <Col className="text-end">
-              <Button>Create Forum</Button>
-            </Col>
-          </Row>
+          <h2>Forums</h2>
+          <Container>
+            <Row className="mt-2">
+              <Col xs={8}>
+                <Searchbar submitFunc={searchBarSubmit} />
+              </Col>
+              <Col className="text-end">
+                <Button onClick={handleModalShow}>Create Forum</Button>
+              </Col>
+            </Row>
+          </Container>
+          <Container className="forumentries-container">
+            <ForumEntry
+              author="Hello"
+              dateCreated="10/10/2023"
+              title="Some Title"
+              subtitle="The subtitle"
+              numPosts="50"
+            />
+            <ForumEntry
+              author="Hello"
+              dateCreated="10/10/2023"
+              title="Some Title"
+              subtitle=""
+              numPosts="50"
+            />
+            <ForumEntry
+              author="Hello"
+              dateCreated="10/10/2023"
+              title="Some Title"
+              subtitle="The subtitle"
+              numPosts="50"
+            />
+          </Container>
         </Container>
-        <Container className="forumentries-container">
-          <ForumEntry
-            author="Hello"
-            dateCreated="10/10/2023"
-            title="Some Title"
-            subtitle="The subtitle"
-            numPosts="50"
-          />
-          <ForumEntry
-            author="Hello"
-            dateCreated="10/10/2023"
-            title="Some Title"
-            subtitle=""
-            numPosts="50"
-          />
-          <ForumEntry
-            author="Hello"
-            dateCreated="10/10/2023"
-            title="Some Title"
-            subtitle="The subtitle"
-            numPosts="50"
-          />
-        </Container>
-      </Container>
-    </div>
+      </div>
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Forum</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ForumCreateForm />
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
